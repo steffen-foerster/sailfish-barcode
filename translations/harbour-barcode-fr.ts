c1025518f92a3601cb7d938d6c50067b9114680e
lutinotmalin <35455719+lutinotmalin@users.noreply.github.com>
2018-02-13 11:11:48 +0100

    Update harbour-barcode-fr.ts
    Refreshed French translation for CodeReader !

--- translations/harbour-barcode-fr.ts
+++ translations/harbour-barcode-fr.ts
@@ -9,15 +9,15 @@
     </message>
     <message>
         <source>This app demonstrates a bar code reader for Sailfish OS. I hope it is useful for other projects. CodeReader is open source and licensed under the MIT License.</source>
-        <translation>Cette application est une démonstration de lecteur de code-barres pour Sailfish OS. CodeReader est un logiciel &quot;open source&quot; sous licence MIT.</translation>
+        <translation>Cette application est un lecteur de code-barre, conçue pour Sailfish OS, avec l&apos;espoir qu&apos;elle soit utile à d&apos;autres projets. CodeReader est un logiciel open source soumis aux conditions de la Licence MIT.</translation>
     </message>
     <message>
         <source>Restriction</source>
-        <translation>Contrainte</translation>
+        <translation>Restriction</translation>
     </message>
     <message>
         <source>This app prevents a scan if the Jolla Camera app is running. This restriction helps to avoid an interference of the Camera app.</source>
-        <translation>Cette application ne scannera pas tant que l&apos;application Jolla Camera est lancée. Cette contrainte permet d&apos;éviter des interférences avec l&apos;application Camera.</translation>
+        <translation>CodeReader ne scannera rien tant que l&apos;application Jolla Caméra sera active. Cette restriction permet d&apos;éviter des interférences avec la caméra, tout simplement.</translation>
     </message>
     <message>
         <source>Version</source>
@@ -41,7 +41,7 @@
     </message>
     <message>
         <source>This project uses code and ideas of other projects, see README.md on Github.</source>
-        <translation>Ce projet utilise des idées et du code provenant d&apos;autres projets. Pour plus d&apos;informations, veuillez lire le fichier README.md sur Github</translation>
+        <translation>Ce projet utilise des idées et du code provenant d&apos;autres projets. Pour plus d&apos;informations, merci de bien vouloir consulter le fichier README.md sur GitHub.</translation>
     </message>
     <message>
         <source>Supported 1D/2D bar codes</source>
@@ -56,15 +56,15 @@
     <name>AutoScanPage</name>
     <message>
         <source>No code detected! Try again.</source>
-        <translation>Aucun code barres détécté ! Veuillez réessayer.</translation>
+        <translation>Aucun code-barre détecté ! Veuillez réessayer.</translation>
     </message>
     <message>
         <source>Please close the Jolla Camera app.</source>
-        <translation>Veuillez fermer l&apos;application Jolla Camera</translation>
+        <translation>Veuillez d'abord fermer l&apos;application Jolla Caméra.</translation>
     </message>
     <message>
         <source>Scanning failed (code: %1)! Try again.</source>
-        <translation>Echec (code : %1) ! Veuillez réessayer.</translation>
+        <translation>Échec (code : %1) ! Veuillez réessayer.</translation>
     </message>
     <message>
         <source>About CodeReader</source>
@@ -84,7 +84,7 @@
     </message>
     <message>
         <source>Scan in progress ...</source>
-        <translation>Scan en cours ...</translation>
+        <translation>Scan en cours...</translation>
     </message>
 </context>
 <context>
@@ -95,11 +95,11 @@
     </message>
     <message>
         <source>Copy to clipboard</source>
-        <translation>Copier dans le presse papier</translation>
+        <translation>Copier dans le presse-papier</translation>
     </message>
     <message>
         <source>History is empty</source>
-        <translation>Historique vide</translation>
+        <translation>Historique vierge</translation>
     </message>
     <message>
         <source>History</source>
@@ -107,34 +107,34 @@
     </message>
     <message>
         <source>Delete all</source>
-        <translation>Supprimer tout</translation>
+        <translation>Tout supprimer</translation>
     </message>
     <message>
         <source>Deleting all</source>
-        <translation>Supprime tout</translation>
+        <translation>Supression intégrale</translation>
     </message>
     <message>
         <source>Deleting</source>
-        <translation>Supprime</translation>
+        <translation>Suppression</translation>
     </message>
 </context>
 <context>
     <name>Settings1View</name>
     <message>
         <source>Detection sound</source>
-        <translation type="unfinished">Bip !</translation>
+        <translation>Signal sonore de détection</translation>
     </message>
     <message>
         <source>Scan on start</source>
-        <translation>Scanner au démarrage</translation>
+        <translation>Scanner au démarrage de l'appli</translation>
     </message>
     <message>
         <source>deactivated</source>
-        <translation>désactivée</translation>
+        <translation>désactivé</translation>
     </message>
     <message>
         <source>%1 items</source>
-        <translation>%1 éléments</translation>
+        <translation>%1 élément(s)</translation>
     </message>
     <message>
         <source>Confirm resize</source>
@@ -146,7 +146,7 @@
     </message>
     <message>
         <source>Max history size (saved values: %1)</source>
-        <translation>Historique maximum (%1 éléments sauvegardés)</translation>
+        <translation>Taille maximale de l'historique (%1 éléments sauvegardés)</translation>
     </message>
     <message>
         <source>History</source>
@@ -165,11 +165,11 @@
     </message>
     <message>
         <source>deactivated</source>
-        <translation>désactivée</translation>
+        <translation>désactivé</translation>
     </message>
     <message>
         <source>%1 seconds</source>
-        <translation>%1 secondes</translation>
+        <translation>%1 seconde(s)</translation>
     </message>
 </context>
 <context>
@@ -184,18 +184,18 @@
     </message>
     <message>
         <source>Scan and history</source>
-        <translation>Lecture et historique</translation>
+        <translation>Scan et historique</translation>
     </message>
 </context>
 <context>
     <name>TextPage</name>
     <message>
         <source>Decoded text</source>
-        <translation>Texte lu</translation>
+        <translation>Texte décodé</translation>
     </message>
     <message>
         <source>Set focus</source>
-        <translation>Mettre au point</translation>
+        <translation>Mise au point</translation>
     </message>
     <message>
         <source>Copy selected text</source>
